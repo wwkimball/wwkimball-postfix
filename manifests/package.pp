34b4f596ef7589382257dd58ffbdebe5481b7223
William Kimball <30981667+wwkimball@users.noreply.github.com>
2018-02-19 10:55:32 -0600

    Enable Postfix 3 style plugins and SCL style naming

--- manifests/package.pp
+++ manifests/package.pp
@@ -26,9 +26,29 @@
 #  postfix::package_ensure: purged
 #
 class postfix::package {
+  $default_plugin_attributes = {
+    ensure  => present,
+    require => Package['postfix'],
+    tag     => [ 'postfix-plugin', ],
+  }
+
   package { 'postfix':
     ensure => $postfix::package_ensure,
     name   => $postfix::package_name,
+  }
+
+  pick($postfix::plugin_packages, {}).each | String $name, Hash $attrs | {
+    if $postfix::package_ensure in ['absent', 'purged'] {
+      package { $name:
+        ensure => $postfix::package_ensure,
+        before => [ Package['postfix'], ],
+      }
+    } else {
+      package {
+        default: *=> $default_plugin_attributes,;
+        $name:   *=> $attrs,;
+      }
+    }
   }
 }
 # vim: syntax=puppet:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:ai
